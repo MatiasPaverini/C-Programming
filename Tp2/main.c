5abf970430ae68f3336d2561ce0417bca3bebf62
Matías Paverini <matiaspaverini@gmail.com>
2016-09-15 19:51:42 -0300

    Tp2

---
+++ Tp2/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+int main()
+{
+    Persona listado[19];
+    char seguir = 's';
+    int opcion = 0;
+    Inicializacion(listado);
+    while (seguir == 's')
+    {
+        printf("1- Agregar persona\n");
+        printf("2- Borrar persona\n");
+        printf("3- Imprimir lista ordenada por nombre\n");
+        printf("4- Imprimir grafico de edades\n");
+        printf("5- Salir\n");
+
+        scanf("%d",&opcion);
+        switch (opcion)
+        {
+            case 1:
+                Agregar_Persona(listado);
+                break;
+            case 2:
+                Borrar_Persona(listado);
+                break;
+            case 3:
+                Imprimir_Lista(listado);
+                break;
+            case 4:
+                Graficar(listado);
+                break;
+            case 5:
+                seguir = 'n';
+                break;
+        }
+    }
+    return 0;
+}
